0f7db3af906ed92a92c394b21e44f741d5a5ce65
mdmansoor001 <78096137+mdmansoor001@users.noreply.github.com>
2024-04-01 11:10:17 +0530

    appointment.py

--- appointment.py
+++ appointment.py
@@ -1,177 +1,136 @@
-# import modules
-try:
-    # for Python2
-    from Tkinter import *   ## notice capitalized T in Tkinter 
-except ImportError:
-    # for Python3
-    from tkinter import *   ## notice lowercase 't' in tkinter here
-    import tkinter as tk
-import sqlite3
+import tkinter as tk
+from tkinter import ttk
 import tkinter.messagebox
+import sqlite3
+from tkcalendar import DateEntry
 
-# connect to the databse.
+# Connect to the database
 conn = sqlite3.connect('database.db')
-# cursor to move in the database
 c = conn.cursor()
 
-# empty list to later appends the ids from the database
-ids = []
-
-# tkinter window
-class App:
+# Tkinter window
+class MyApp:
     def __init__(self, master):
         self.master = master
+        self.master.title("Location Selection")
+        self.master.geometry("1000x620+100+50")  # Resolution of the window
+        self.master.resizable(True, False)  # Preventing the resize feature
 
-        # creating the format in master
-        self.left = Frame(master, width=600, height=720, bg='lightblue')
-        self.left.pack(side = LEFT)
+        # Canvas setup
+        self.canvas = tk.Canvas(master, width=1000, height=620, bg='lightblue')
+        self.canvas.pack()
 
-        self.right = Frame(master, width=400, height=720, bg='steelblue')
-        self.right.pack(side = RIGHT)
+        # Background Image
+        self.bg_image = tk.PhotoImage(file="resources/images123.png")
+        self.canvas.create_image(0, 0, anchor=tk.NW, image=self.bg_image)
 
-        # heading
-        self.heading = Label(self.left, text="Enter patient details", font=('arial 18'), fg='black', bg='lightblue')
-        self.heading.place(x=220, y=50)
+        # Heading
+        self.heading = tk.Label(self.canvas, text="Enter patient details", font=('arial 18'), fg='black', bg='lightblue')
+        self.heading.place(x=400, y=50)
 
-        # patient's name
-        self.name = Label(self.left, text="Patient's Name", font=('arial 12'), fg='black', bg='lightblue')
-        self.name.place(x=65, y=100)
+        # Patient's name
+        self.name_label = tk.Label(self.canvas, text="Patient's Name", font=('arial 12'), fg='black', bg='lightblue')
+        self.name_label.place(x=300, y=100)
+        self.name_var = tk.StringVar()
+        self.name_ent = tk.Entry(self.canvas, width=30, textvariable=self.name_var)
+        self.name_ent.place(x=450, y=100)
+        self.name_var.trace_add("write", self.validate_name)  # Event binding for real-time validation
 
-        # age
-        self.age = Label(self.left, text="Age", font=('arial 12'), fg='black', bg='lightblue')
-        self.age.place(x=65, y=140)
+        # Age
+        self.age_label = tk.Label(self.canvas, text="Age", font=('arial 12'), fg='black', bg='lightblue')
+        self.age_label.place(x=300, y=140)
+        self.age_var = tk.StringVar()
+        self.age_ent = tk.Entry(self.canvas, width=30, textvariable=self.age_var)
+        self.age_ent.place(x=450, y=140)
+        self.age_var.trace_add("write", self.validate_age)  # Event binding for real-time validation
 
-        # gender
-        self.gender = Label(self.left, text="Gender", font=('arial 12'), fg='black', bg='lightblue')
-        self.gender.place(x=65, y=180)
+        # Gender
+        self.gender_label = tk.Label(self.canvas, text="Gender", font=('arial 12'), fg='black', bg='lightblue')
+        self.gender_label.place(x=300, y=180)
+        self.gender_var = tk.StringVar()
+        self.gender_var.set("Male")  # Default value
+        self.gender_radio_male = tk.Radiobutton(self.canvas, text="Male", variable=self.gender_var, value="Male", font=('arial', 11), bg='lightblue')
+        self.gender_radio_male.place(x=450, y=180)
+        self.gender_radio_female = tk.Radiobutton(self.canvas, text="Female", variable=self.gender_var, value="Female", font=('arial', 11), bg='lightblue')
+        self.gender_radio_female.place(x=510, y=180)
+        self.gender_radio_trans = tk.Radiobutton(self.canvas, text="Transgender", variable=self.gender_var, value="Transgender", font=('arial', 11), bg='lightblue')
+        self.gender_radio_trans.place(x=590, y=180)
 
-        #Date
-        self.Date = Label(self.left, text="Date(dd/mm/yyyy)", font=('arial 12'), fg='black', bg='lightblue')
-        self.Date.place(x=65, y=140)
+        # Date
+        self.date_label = tk.Label(self.canvas, text="Date", font=('arial 12'), fg='black', bg='lightblue')
+        self.date_label.place(x=300, y=220)
+        self.date_ent = DateEntry(self.canvas, width=27, date_pattern='dd/mm/yyyy')
+        self.date_ent.place(x=450, y=220)
 
-        # location
-        self.location = Label(self.left, text="Location", font=('arial 12'), fg='black', bg='lightblue')
-        self.location.place(x=65, y=220)
+        # Location
+        self.location_label = tk.Label(self.canvas, text="Location", font=('arial 12'), fg='black', bg='lightblue')
+        self.location_label.place(x=300, y=260)
+        self.location_var = tk.StringVar()
+        self.location_var.set("Select City")  # Default value
+        self.location_combobox = ttk.Combobox(self.canvas, textvariable=self.location_var, values=["Chennai", "Trichy","Thanjavur", "Madurai", "Pudukkottai"])
+        self.location_combobox.config(width=15, font=('arial', 11))
+        self.location_combobox.place(x=450, y=260)
 
-        # appointment time
-        self.time = Label(self.left, text="Appointment Time (HH:MM)", font=('arial 12'), fg='black', bg='lightblue')
-        self.time.place(x=65, y=260)
-        # phone
-        self.phone = Label(self.left, text="Phone Number", font=('arial 12'), fg='black', bg='lightblue')
-        self.phone.place(x=65, y=300)
+        # Appointment time
+        self.time_label = tk.Label(self.canvas, text="Time (HH:MM)", font=('arial 12'), fg='black', bg='lightblue')
+        self.time_label.place(x=300, y=300)
+        self.time_ent = tk.Entry(self.canvas, width=30)
+        self.time_ent.place(x=450, y=300)
 
-        # Enteries for all labels...
-        self.name_ent = Entry(self.left, width=30)
-        self.name_ent.place(x=275, y=100)
+        # Phone number
+        self.phone_label = tk.Label(self.canvas, text="Phone Number", font=('arial 12'), fg='black', bg='lightblue')
+        self.phone_label.place(x=300, y=340)
+        self.phone_ent = tk.Entry(self.canvas, width=30)
+        self.phone_ent.place(x=450, y=340)
 
-        self.age_ent = Entry(self.left, width=30)
-        self.age_ent.place(x=275, y=140)
+        # Button to add appointment
+        self.submit = tk.Button(self.canvas, text="Add Appointment", width=20, height=2, bg='steelblue', command=self.add_appointment)
+        self.submit.place(x=300, y=390)
 
-        # gender list
-        GenderList = ["Male",
-        "Female",
-        "Transgender"]
+        # Button to go back home
+        self.back_home = tk.Button(self.canvas, text="Back Home", width=20, height=2, bg='steelblue', command=root.destroy)
+        self.back_home.place(x=490, y=390)
 
-        # Option menu
-        self.var = tk.StringVar()
-        self.var.set(GenderList[0])
-
-        self.opt = tk.OptionMenu(self.master, self.var, *GenderList)
-        self.opt.config(width=10, font=('arial', 11))
-        self.opt.place(x=275, y=180)
-
-        # callback method
-
-        def callback(*args):
-            for i in range(len(GenderList)):
-                if GenderList[i] == self.var.get():
-                    self.gender_ent = GenderList[i]
-                    break
-        
-        self.var.trace("w", callback)
-
-        self.location_ent = Entry(self.left, width=30)
-        self.location_ent.place(x=275, y=220)
-
-        self.time_ent = Entry(self.left, width=30)
-        self.time_ent.place(x=275, y=260)
-
-        self.phone_ent = Entry(self.left, width=30)
-        self.phone_ent.place(x=275, y=300)
-
-        # button to perform a command
-        self.submit = Button(self.left, text="Add Appointment", width=20, height=2, bg='steelblue', command=self.add_appointment)
-        self.submit.place(x=190, y=350)
-
-        #back button
-            
-        self.submit = Button(self.left, text="Back Home", width=20, height=2, bg='steelblue', command=root.destroy)
-        self.submit.place(x=350, y=350)
-
-
-        # getting the number of appointments fixed to view in the log
-        sql2 = "SELECT ID FROM appointments "
-        self.result = c.execute(sql2)
-        for self.row in self.result:
-            self.id = self.row[0]
-            ids.append(self.id)
-
-        # ordering the ids
-        self.new = sorted(ids)
-        self.final_id = self.new[len(ids)-1]
-
-        # displaying the logs in right frame
-        self.logs = Label(self.right, text="Appointment Log", font=('arial 20 bold'), fg='white', bg='steelblue')
-        self.logs.place(x=20, y=10)
-
-        self.box = Text(self.right, font=('courier 11'), width=40, height=30)
-        self.box.place(x=20, y=60)
-        self.box.insert(END, "Total Appointments till now :  " + str(self.final_id))
-
-    # function to call when the submit button is clicked
+    # Function to call when the submit button is clicked
     def add_appointment(self):
-        # getting the user inputs
+        # Getting the user inputs
         self.val1 = self.name_ent.get()
         self.val2 = self.age_ent.get()
-        self.val3 = self.gender_ent
-        self.val4 = self.location_ent.get()
+        self.val3 = self.gender_var.get()
+        self.val4 = self.location_var.get()
         self.val5 = self.time_ent.get()
         self.val6 = self.phone_ent.get()
 
-
-        
-
-        # checking if the user input is empty
-        if self.val1 == '' or self.val2 == '' or self.val3 == '' or self.val4 == '' or self.val5 == '' or self.val6 == '':
-            tkinter.messagebox.showwarning("Warning","Please fill up all the details")
+        # Checking for input validation
+        if not all([self.val1, self.val2, self.val3, self.val4, self.val5, self.val6]):
+            tkinter.messagebox.showwarning("Warning", "Please fill up all the details")
+        elif not self.val2.isdigit():
+            tkinter.messagebox.showwarning("Warning", "Age must be a number")
+        elif not self.val6.isdigit() or len(self.val6) != 10:
+            tkinter.messagebox.showwarning("Warning", "Phone number must be a 10-digit number")
         else:
-            # now we add to the database
+            # Inserting data into the database
             sql = "INSERT INTO 'appointments' (name, age, gender, location, scheduled_time, phone) VALUES(?, ?, ?, ?, ?, ?)"
             c.execute(sql, (self.val1, self.val2, self.val3, self.val4, self.val5, self.val6))
             conn.commit()
-            tkinter.messagebox.showinfo("Success","Appointment for "+str(self.val1)+" has been created")
-            
+            tkinter.messagebox.showinfo("Success", f"Appointment for {self.val1} has been created")
 
-            self.box.insert(END, '\nAppointment fixed for ' + str(self.val1) + ' at ' + str(self.val5))
+    # Function to validate name in real-time
+    def validate_name(self, *args):
+        if not self.name_var.get().isalpha():
+            tkinter.messagebox.showerror("ERROR", "Name must contain only alphabetic characters")
 
+    # Function to validate age in real-time
+    def validate_age(self, *args):
+        if not self.age_var.get().isdigit():
+            tkinter.messagebox.showerror("ERROR", "Age must be a number")
 
-#creating the object
+# Creating the object
 root = tk.Tk()
-b = App(root)
+app = MyApp(root)
 
-# resolution of the window
-root.geometry("1000x620+100+50")
-
-# preventing the resize feature
-root.resizable(False, False)
-
-# title of the window
+# Title of the window
 root.title("Add new appointment")
 
-# icon of the application
-root.iconphoto(False, tk.PhotoImage(file='resources/icon.png'))
-
-root.bind('<Return>', b.add_appointment)
-
-# end the loop
+# End the loop
 root.mainloop()
